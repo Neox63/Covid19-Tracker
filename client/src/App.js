51531602ff0f20a129ac6755e69a94975c6efad1
Neox63 <matchambon60@gmail.com>
2020-12-19 13:16:02 +0100

    Deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { React, useState, useEffect, Fragment } from 'react';
+import { React, useState, useEffect } from 'react';
 import Axios from 'axios';
 import StatItem from './StatItem';
 import Button from './Button';
@@ -22,28 +22,28 @@ const App = () => {
     return ( 
         <div className = "App">
             <h1>Global Covid19 Tracker</h1>
-            {(typeof data.Global != "undefined") ? ( <p>Dernière mise à jour à { data.Date.substring(11, 16) }.</p> ) : ('Récupération des données en cours...')} 
+            {(typeof data.Global != "undefined") ? ( <p>Dernière mise à jour à { data.Date.substring(11, 16) }.</p> ) : ( <p>Loading...</p>)} 
             
             <div className = "itemContainer">
                 <StatItem 
                     color = "#e1f3ff" 
                     title = "Nombre de cas" 
-                    statTotal = {(typeof data.Global != "undefined") ? ( data.Global.TotalConfirmed.toLocaleString() ) : ('Récupération des données en cours...')} 
-                    statNew = {(typeof data.Global != "undefined") ? ( data.Global.NewConfirmed.toLocaleString() ) : ('Récupération des données en cours...')}  
+                    statTotal = {(typeof data.Global != "undefined") ? ( data.Global.TotalConfirmed.toLocaleString() ) : ('Loading...')} 
+                    statNew = {(typeof data.Global != "undefined") ? ( data.Global.NewConfirmed.toLocaleString() ) : ('Loading...')}  
                 />
 
                 <StatItem 
                     color = "#ffb5b6" 
                     title = "Nombre de décès" 
-                    statTotal = {(typeof data.Global != "undefined") ? ( data.Global.TotalDeaths.toLocaleString() ) : ('Récupération des données en cours...')} 
-                    statNew = {(typeof data.Global != "undefined") ? ( data.Global.NewDeaths.toLocaleString() ) : ('Récupération des données en cours...')}  
+                    statTotal = {(typeof data.Global != "undefined") ? ( data.Global.TotalDeaths.toLocaleString() ) : ('Loading...')} 
+                    statNew = {(typeof data.Global != "undefined") ? ( data.Global.NewDeaths.toLocaleString() ) : ('Loading...')}  
                 />
 
                 <StatItem 
                     color="#b7ffe8" 
                     title="Guéris" 
-                    statTotal = {(typeof data.Global != "undefined") ? ( data.Global.TotalRecovered.toLocaleString() ) : ('Récupération des données en cours...')} 
-                    statNew = {(typeof data.Global != "undefined") ? ( data.Global.NewRecovered.toLocaleString() ) : ('Récupération des données en cours...')}  
+                    statTotal = {(typeof data.Global != "undefined") ? ( data.Global.TotalRecovered.toLocaleString() ) : ('Loading...')} 
+                    statNew = {(typeof data.Global != "undefined") ? ( data.Global.NewRecovered.toLocaleString() ) : ('Loading...')}  
                 />
             </div>
 
